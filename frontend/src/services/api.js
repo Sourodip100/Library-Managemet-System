4011f3b03beacbb89d0ce901b361f06bda089d87
Sourodip Ghosh <sourodipghosh100@gmail.com>
2026-07-19 03:08:14 +0530

    Fix frontend API URL for EKS deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || "/api";
+// const API_URL = process.env.REACT_APP_API_URL || "/api";
+const API_URL = 'http://k8s-librarys-libraryi-f993b41b66-1255817909.ap-south-1.elb.amazonaws.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
